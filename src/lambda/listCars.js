8be821e41e6aca968e0444fdf57f014ce301a4df
Mustafa <m.amaragy@gmail.com>
2026-03-25 13:10:47 +0300

    Update listCars.js

--- src/lambda/listCars.js
+++ src/lambda/listCars.js
@@ -13,7 +13,7 @@ exports.handler = async () => {
         let items = data.Items;
         if (!items || items.length === 0) {
             items = [{
-                id: "dummy1020",
+                id: "dummy10880",
                 name: "",
                 description: "",
                 isEmpty: true
