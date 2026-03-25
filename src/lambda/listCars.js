c3fae23a6cc211c9a5c7fb8546e9c628f3b1de4e
Mustafa <m.amaragy@gmail.com>
2026-03-25 15:44:37 +0300

    Change default car ID from 'EEER' to 'EEEiiR'

--- src/lambda/listCars.js
+++ src/lambda/listCars.js
@@ -13,7 +13,7 @@ exports.handler = async () => {
         let items = data.Items;
         if (!items || items.length === 0) {
             items = [{
-                id: "EEER",
+                id: "EEEiiR",
                 name: "",
                 description: "",
                 isEmpty: true
