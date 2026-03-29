c8d9c78006a54306925dd840f5ecdd4e9f12c47e
Mustafa <m.amaragy@gmail.com>
2026-03-29 09:35:33 +0300

    Update default car ID in listCars function

--- src/lambda/listCars.js
+++ src/lambda/listCars.js
@@ -13,7 +13,7 @@ exports.handler = async () => {
         let items = data.Items;
         if (!items || items.length === 0) {
             items = [{
-                id: "EwET7ss7iR",
+                id: "EwETFFFFF7ss7iR",
                 name: "",
                 description: "",
                 isEmpty: true
